a6a8b0231a6819b2ba39605b23cf4c5337907137
Abralava Georgii <georgijabralava@gmail.com>
2024-09-13 19:17:25 +0300

    Переделал tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeDataStyle.light,
-      home: CustomTabBar(),
+      home: const CustomTabBar(),
     );
   }
 }
-
 
 class CustomTabBar extends StatelessWidget {
   const CustomTabBar({super.key});
@@ -29,19 +28,50 @@ class CustomTabBar extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: Text('1 января 09:00', style: Theme.of(context).textTheme.headlineLarge,),
+          title: Text(
+            '1 января 09:00',
+            style: Theme.of(context).textTheme.headlineLarge,
+          ),
           centerTitle: true,
-          actions: [Icon(Icons.calendar_month)],
-          bottom: const TabBar(
-          tabs: [
-            Tab(
-              text: 'Дневник настроения',
+          actions: const [Icon(Icons.calendar_month)],
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(30),
+            child: Container(
+              height: 30,
+              margin: EdgeInsets.only(left: 22, right: 22),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(242, 242, 242, 1),
+                borderRadius: BorderRadius.circular(48)
+              ),
+              child: TabBar(
+                dividerColor: Colors.transparent,
+                splashBorderRadius: BorderRadius.circular(48),
+                indicator: BoxDecoration(
+                  color: Color.fromRGBO(255, 135, 2, 1),
+                  borderRadius: BorderRadius.circular(48),
+                ),
+                indicatorSize: TabBarIndicatorSize.tab,
+                labelStyle: Theme.of(context).textTheme.labelMedium,
+                unselectedLabelStyle: Theme.of(context).textTheme.labelSmall,
+                tabs: [
+                  Tab(
+                    icon: Icon(
+                      Icons.book,
+                      size: 12,
+                    ),
+                    text: 'Дневник настроения',
+                  ),
+                  Tab(
+                    icon: Icon(
+                      Icons.bar_chart,
+                      size: 12,
+                    ),
+                    text: 'Статистика',
+                  )
+                ],
+              ),
             ),
-            Tab(
-              text: 'Статистика',
-            )
-          ],
-        ),
+          ),
         ),
         body: const TabBarView(children: [HomeScreen(), StatisticsScreen()]),
       ),
