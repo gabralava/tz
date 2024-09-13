ae9b8b3d654487d3e124f4ff0408f5f1039f9f00
Abralava Georgii <georgijabralava@gmail.com>
2024-09-13 19:22:52 +0300

    Добавил возможность раскрыть меню эмоций

--- lib/presentation/ui/emotion_card.dart
+++ lib/presentation/ui/emotion_card.dart
@@ -3,36 +3,59 @@ import 'package:flutter/material.dart';
 class EmotionCard extends StatelessWidget {
   final String emotionStatus;
   final String imagePath;
-  const EmotionCard(
-      {super.key, required this.emotionStatus, required this.imagePath});
+  final bool isExpanded;
+  final ValueChanged<int> onTap;
+  final int index;
+
+  const EmotionCard({
+    super.key,
+    required this.emotionStatus,
+    required this.imagePath,
+    required this.isExpanded,
+    required this.onTap,
+    required this.index,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(right: 12),
-      child: Container(
-        height: 118,
-        width: 83,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(64),
-          color: Colors.white,
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromRGBO(182, 161, 192, 0.11),
-              offset: Offset(2, 4),
-              blurRadius: 10.8,
-              spreadRadius: 0,
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () => onTap(index),
+          child: Padding(
+            padding: const EdgeInsets.only(right: 12),
+            child: Container(
+              height: 118,
+              width: 83,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(64),
+                border: isExpanded
+                    ? Border.all(color: Colors.orange, width: 2)
+                    : null,
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromRGBO(182, 161, 192, 0.11),
+                    offset: Offset(2, 4),
+                    blurRadius: 10.8,
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(imagePath),
+                  Text(
+                    emotionStatus.substring(14).replaceAll('.png', ''),
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ],
+              ),
             ),
-          ],
+          ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(imagePath),
-            Text(emotionStatus.substring(14).replaceAll('.png', ''), style: Theme.of(context).textTheme.titleMedium,),
-          ],
-        ),
-      ),
+      ],
     );
   }
 }
